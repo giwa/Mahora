ee4e636238bc51c4ad8757a189548b447fc27ccf
giwa <ugw.gi.world@gmail.com>
2013-04-01 01:46:36 +0900

    modify total environment

--- controller/controller.cpp
+++ controller/controller.cpp
@@ -5,6 +5,9 @@
 #include <cstdio>
 #include <stdio.h>
 #include <string.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
 
 using namespace std;
 using namespace kyototycoon;
@@ -15,63 +18,81 @@ struct RouterTableInfo{
 	int port;
 };
 
-// main routine
-int main(int argc, char** argv) {
-
-if(argc < 3){
-	cout << "sourceIP ForwardingIP s" << endl;
-	return 0;
-}
-
-  // create the database object
-  RemoteDB db;
-
-  // open the database
-  if (!db.open()) {
-    cerr << "open error: " << db.error().name() << endl;
-  }
-
-
+int UpdateRoutingTable(RemoteDB *db, string src_ip, string nexthop_ip, int nexthop_port){
 	RouterTableInfo *rtinfo = new RouterTableInfo;
-	string dst_ip,src_ip;
-	int port;
 	char *buf;
 
-	src_ip = argv[1];
-	dst_ip = argv[2];
-	port = 52000;
-
-	memcpy(rtinfo->nexthop, dst_ip.c_str(), dst_ip.size());
-	rtinfo->ip_len = dst_ip.size();
-	rtinfo->port = port;
-	cout << rtinfo->ip_len << endl;
+	memcpy(rtinfo->nexthop, nexthop_ip.c_str(), nexthop_ip.size());
+	rtinfo->ip_len = nexthop_ip.size();
+	rtinfo->port = nexthop_port;
 	buf = (char *)rtinfo;
-	db.set(src_ip.c_str(), src_ip.size(), buf, sizeof(RouterTableInfo) );
+	db->set(src_ip.c_str(), src_ip.size(), buf, sizeof(RouterTableInfo));
+	delete rtinfo;
+}
 
+int ShowRoutingTable(RemoteDB *db){
+	RemoteDB::Cursor* cur = db->cursor();
+
+	cur->jump();
+	string ckey, cvalue;
+	cout << "src_ip" << "	" << "nexthop_ip" << "	" << "nexthop_port" << endl;
+	RouterTableInfo *rtinfo;
+	while (cur->get(&ckey, &cvalue, NULL, true)) {
+		rtinfo = (RouterTableInfo *)cvalue.c_str();
+		cout << ckey << "	" << rtinfo->nexthop << "	" << rtinfo->port  <<endl;
+	}
+	delete cur;
+
+}
+
+
+
+// main routine
+int main(int argc, char** argv) {
+/*
+	if(argc < 3){
+	cout << "sourceIP ForwardingIP" << endl;
+	return 0;
+	}
+*/
+    char *ptr;
+    string line;
+    while(ptr = readline())
+    {
+		
+        line = ptr;
+        cout << line << endl;
+        free(ptr);
+    }
+
+
+	// create the database object
+	RemoteDB db;
+
+	// open the database
+	if (!db.open()) {
+	cerr << "open error: " << db.error().name() << endl;
+	}
+
+	string nexthop_ip,src_ip;
+	int nexthop_port;
+
+	src_ip = argv[1];
+	nexthop_ip = argv[2];
+	nexthop_port = 52000;
+	UpdateRoutingTable(&db, src_ip, nexthop_ip, nexthop_port);
 
 	src_ip = argv[2];
-	dst_ip = argv[1];
-	port = 52000;
-	memcpy(rtinfo->nexthop, dst_ip.c_str(), dst_ip.size());
-	rtinfo->ip_len = dst_ip.size();
-	rtinfo->port = port;
-	buf = (char *)rtinfo;
-	db.set(src_ip.c_str(), src_ip.size(), buf, sizeof(RouterTableInfo) );
+	nexthop_ip = argv[1];
+	nexthop_port = 52000;
+	UpdateRoutingTable(&db, src_ip, nexthop_ip, nexthop_port);
 
-  // traverse records
-  /*
-  RemoteDB::Cursor* cur = db.cursor();
-  cur->jump();
-  string ckey, cvalue;
-  while (cur->get(&ckey, &cvalue, NULL, true)) {
-    cout << ckey << ":" << cvalue << endl;
-  }
-  delete cur;
-*/
-  // close the database
-  if (!db.close()) {
-    cerr << "close error: " << db.error().name() << endl;
-  }
+	ShowRoutingTable(&db);
 
-  return 0;
+	// close the database
+	if (!db.close()) {
+		cerr << "close error: " << db.error().name() << endl;
+	}
+
+	return 0;
 }
